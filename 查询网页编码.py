9000b54b17eb3442b52e7a03186a145f2f488393
ZhBerlin <48012458+ZhBerlin@users.noreply.github.com>
2019-09-29 21:20:55 +0800

    Add files via upload

---
+++ 查询网页编码.py
@@ -0,0 +1,17 @@
+import urllib.request
+import chardet
+
+def main():
+    url = input('请输入URL：')
+
+    response = urllib.request.urlopen(url)
+    html = response.read()
+
+    encode = chardet.detect(html)['encoding']
+    if encode =='GB2312':
+        encode = 'GBK'
+
+    print('该网页使用的编码是：%s'%encode)
+
+if __name__ =='main':
+    main()
